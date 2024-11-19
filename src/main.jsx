470af6f315791e3d70cbfa47f930803cc43e5587
mrproject1996 <mj.reichenberg@gmail.com>
2024-11-19 00:42:35 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import BudgetCalculator from './BudgetCalculator.jsx'
+import BudgetCalculator from './BudgetCalculator'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BudgetCalculator />
+    <div className="min-h-screen bg-slate-50 py-8">
+      <BudgetCalculator />
+    </div>
   </React.StrictMode>,
 )
