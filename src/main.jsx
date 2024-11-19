f8aed950ca0189e76a9f10e5d666b4a293ad9979
mrproject1996 <mj.reichenberg@gmail.com>
2024-11-19 00:14:49 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BudgetCalculator from './BudgetCalculator.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BudgetCalculator />
+  </React.StrictMode>,
+)
